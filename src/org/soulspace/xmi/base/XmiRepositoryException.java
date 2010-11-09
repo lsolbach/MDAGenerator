66509a0d6d4e9c4fe68828a8df58c36132d44029
Ludger Solbach <soulman@soulspace.org>
2010-11-09 20:32:59 +0000

    throw XmiRepositoryException with on xmi loading problems

---
+++ src/org/soulspace/xmi/base/XmiRepositoryException.java
@@ -0,0 +1,20 @@
+package org.soulspace.xmi.base;
+
+public class XmiRepositoryException extends RuntimeException {
+
+	public XmiRepositoryException() {
+	}
+
+	public XmiRepositoryException(String message) {
+		super(message);
+	}
+
+	public XmiRepositoryException(Throwable cause) {
+		super(cause);
+	}
+
+	public XmiRepositoryException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
