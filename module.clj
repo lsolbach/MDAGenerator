294b78ed4a68a93abb188a03f4bafc8fb7af3e58
Ludger Solbach <soulman@soulspace.org>
2012-11-21 07:50:57 +0000

    build refactorings, license and readme files added

---
+++ module.clj
@@ -0,0 +1,11 @@
+[
+ :name "DomainMetaModel"
+ :project "org.soulspace.modelling"
+ :type "framework"
+ :version "0.2.0"
+ :description "Extension of the meta model of the model repository."
+ :vendor "soulspace.org"
+ :plugins ["global" "deps" "java" "package"]
+ :dependencies [["org.soulspace.modelling" "ModelRepository" "1.0.0"]
+                ["org.junit" "junit" "3.8.1" "dev"]]
+ ]
---
+++ readme.txt
@@ -0,0 +1,13 @@
+DomainMetaModel
+===============
+
+License
+-------
+Eclipse Public License 1.0 (http://www.eclipse.org/legal/epl-v10.html)
+
+History
+-------
+
+Version 0.2.0 (--.--.201-)
+--------------------------
+
