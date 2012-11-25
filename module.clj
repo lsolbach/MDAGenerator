06429239b8bbad43e513e7c25a4d4f161e062788
Ludger Solbach <soulman@soulspace.org>
2012-11-25 17:37:50 +0000

    updated module.clj

--- module.clj
+++ module.clj
@@ -1,10 +1,12 @@
 [
  :name "DomainMetaModel"
  :project "org.soulspace.modelling"
+ :project-lead "Ludger Solbach"
+ :vendor "soulspace.org"
  :type "framework"
  :version "0.2.0"
  :description "Extension of the meta model of the model repository."
- :vendor "soulspace.org"
+ :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :plugins ["global" "deps" "java" "package"]
  :dependencies [["org.soulspace.modelling" "ModelRepository" "1.0.0"]
                 ["org.junit" "junit" "3.8.1" "dev"]]
