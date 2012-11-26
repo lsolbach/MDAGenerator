a72722a6305164f4a782c3382ea6edb7efebfc4b
Ludger Solbach <soulman@soulspace.org>
2012-11-26 07:51:08 +0000

    updated project files

---
+++ module.clj
@@ -0,0 +1,14 @@
+[
+ :name "ModelRepository"
+ :project "org.soulspace.modelling"
+ :project-lead "Ludger Solbach"
+ :vendor "soulspace.org"
+ :type "component"
+ :version "1.0.0"
+ :description "Model repository for model driven software development."
+ :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
+ :plugins ["global" "deps" "java" "package"]
+ :dependencies [["org.soulspace.modelling" "XmiRepository" "1.0.0"]
+                ["junit" "junit" "3.8.1" "dev"]
+                ]
+ ]
