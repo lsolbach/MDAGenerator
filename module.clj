632e88ac32df88916b394dd17dc94c5d43370010
Ludger Solbach <soulman@soulspace.org>
2012-11-28 10:45:57 +0000

    updated project files

--- module.clj
+++ module.clj
@@ -10,6 +10,6 @@
  This module servers as an example for the extensibility of the meta model."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :plugins ["global" "deps" "java" "package"]
- :dependencies [["org.soulspace.modelling" "ModelRepository" "1.0.0" "dev"]
-                ["org.soulspace.modelling" "XmiRepository" "1.0.0" "dev"]]
+ :dependencies [["org.soulspace.mda" "ModelRepository" "1.0.0" "dev"]
+                ["org.soulspace.mda" "XmiRepository" "1.0.0" "dev"]]
  ]
