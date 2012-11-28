1729a024ae9ba52b33375f4700a441ded45ab372
Ludger Solbach <soulman@soulspace.org>
2012-11-28 11:32:51 +0000

    updated project files

---
+++ TODOS.md
@@ -0,0 +1,5 @@
+TODO's
+======
+
+* Implement IAssociationClass with factory/builder support
+* Filter overridden methods, attributes and associations in getAll*() methods 
--- module.clj
+++ module.clj
@@ -1,14 +1,14 @@
 [
  :name "ModelRepository"
- :project "org.soulspace.modelling"
+ :project "org.soulspace.mda"
  :project-lead "Ludger Solbach"
  :vendor "soulspace.org"
- :type "component"
+ :type "framework"
  :version "1.0.0"
  :description "Model repository for model driven software development."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :plugins ["global" "deps" "java" "package"]
- :dependencies [["org.soulspace.modelling" "XmiRepository" "1.0.0"]
-                ["junit" "junit" "3.8.1" "dev"]
-                ]
+ :dependencies [["org.soulspace.mda" "XmiRepository" "1.0.0"]
+                ["org.exolab" "castor" "0.9.5.3" "runtime" "castor-xml"]
+                ["junit" "junit" "3.8.1" "dev"]]
  ]
