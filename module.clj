6f0cc79ecd095907470361d88989846994c56704
Ludger Solbach <soulman@soulspace.org>
2013-07-23 00:16:23 +0200

    refactored module.clj

--- module.clj
+++ module.clj
@@ -1,5 +1,5 @@
 [
- :name "ModelRepository"
+ :module "ModelRepository"
  :project "org.soulspace.mda"
  :project-lead "Ludger Solbach"
  :vendor "soulspace.org"
@@ -7,9 +7,9 @@
  :version "1.0.0"
  :description "Model repository for model driven software development."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "deps" "java" "junit" "package"]
- :dependencies [["org.soulspace.mda" "XmiRepository" "1.0.0"]
-                ["org.exolab" "castor" "0.9.5.3" "runtime" "castor-xml"]
-                ["xerces" "xercesImpl" "2.9.1"]
-                ["junit" "junit" "3.8.1" "dev"]]
+ :plugins ["global" "sdeps" "java" "junit" "package"]
+ :dependencies [[["org.soulspace.mda" "XmiRepository" "1.0.0"]]
+                [["org.exolab" "castor" "0.9.5.3" "castor-xml"]]
+                [["xerces" "xercesImpl" "2.9.1"]]
+                [["junit" "junit" "3.8.1"] "dev"]]
  ]
