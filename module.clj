2b150dea71d224804c7d8af836aae142e2fe59a3
Ludger Solbach <soulman@soulspace.org>
2013-07-23 00:15:31 +0200

    added project files

---
+++ module.clj
@@ -0,0 +1,10 @@
+[
+ :module "MdaStandardTemplateLibrary"
+ :project "org.soulspace.modelling"
+ :type "data"
+ :version "1.0.0"
+ :description "Templates for model driven software development"
+ :author "Ludger Solbach"
+ :provider "soulspace.org"
+ :plugins ["global" "sdeps" "package"] 
+ ]
