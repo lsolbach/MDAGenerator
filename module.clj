851e9bfa4e10e9a2cf41d8ed11b3d01d64741629
Ludger Solbach <soulman@soulspace.org>
2013-07-31 09:11:20 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -7,9 +7,7 @@
  :version "1.0.1"
  :description "Model repository of the MDA Generator Framework."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "sdeps" "java" "junit" "package"]
- :dependencies [[["org.soulspace.mda" "MDAXmiRepository" "1.0.0"]]
-                [["org.exolab" "castor" "0.9.5.3" "castor-xml"]]
-                [["xerces" "xercesImpl" "2.9.1"]]
+ :plugins ["global" "sdeps" "depsdot" "java" "junit" "package"]
+ :dependencies [[["org.soulspace.mda" "MDAXmiRepository" "1.0.1"]]
                 [["junit" "junit" "3.8.1"] :dev]]
  ]
