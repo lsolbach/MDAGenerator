66cf85f5721f59607b1cc433fad240258c137746
Ludger Solbach <soulman@soulspace.org>
2013-09-03 08:56:35 +0200

    fixed project ids

--- module.clj
+++ module.clj
@@ -1,6 +1,6 @@
 [
  :module "MDARepository"
- :project "org.soulspace.mda"
+ :project "org.soulspace.modelling"
  :project-lead "Ludger Solbach"
  :provider "soulspace.org"
  :type :framework
@@ -8,6 +8,6 @@
  :description "Model repository of the MDA Generator Framework."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :plugins ["global" "dependencies" "java" "junit" "package" "release"]
- :dependencies [[["org.soulspace.mda" "MDAXmiRepository" "1.0.1"]]
+ :dependencies [[["org.soulspace.modelling" "MDAXmiRepository" "1.0.1"]]
                 [["junit" "junit" "3.8.1"] :dev]]
  ]
