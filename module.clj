7bcff763e7892a429cb968d95261418fb619176e
soulman <soulman@pluto>
2014-11-11 10:58:45 +0100

    updated module.clj

--- module.clj
+++ module.clj
@@ -2,17 +2,21 @@
  :module "MDAGenerator"
  :project "org.soulspace.modelling"
  :project-lead "Ludger Solbach"
- :vendor "soulspace.org"
+ :provider "soulspace.org"
  :type :framework
  :version "1.0.1"
  :description "Ant task and generators of the MDA generator framework for model driven software development."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "dependencies" "java" "package" "release"]
- :dependencies [[["org.soulspace.template" "TemplateEngine" "1.0.1"]]
-                [["org.soulspace.common" "JavaUtilLibrary" "0.3.0"]]
-                [["org.soulspace.modelling" "MDARepository" "1.0.1"]]
-                [["org.soulspace.modelling" "MDAXmiRepository" "1.0.1"]]
-                [["org.apache.ant" "ant" "1.8.3"]]
-                [["org.apache.ant" "ant-testutil" "1.8.3"] :dev]
-                [["junit" "junit" "3.8.1"] :dev]]
+ :plugins ["global"
+           ["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/JavaPlugin"]
+           ["org.soulspace.baumeister/JUnitPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+ :dependencies [["org.soulspace.template/TemplateEngine, 1.0.1"]
+                ["org.soulspace.common/JavaUtilLibrary, 0.3.0"]
+                ["org.soulspace.modelling/MDARepository, 1.0.1"]
+                ["org.soulspace.modelling/MDAXmiRepository, 1.0.1"]
+                ["org.apache.ant/ant, 1.8.3"]
+                ["org.apache.ant/ant-testutil, 1.8.3" :dev]
+                ["junit/junit, 3.8.1" :dev]]
  ]
