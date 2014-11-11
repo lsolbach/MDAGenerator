7b0ec736e1304e9026451fe09cc2d1b2e7a7e853
soulman <soulman@pluto>
2014-11-11 10:59:44 +0100

    updated module.clj

--- module.clj
+++ module.clj
@@ -7,8 +7,13 @@
  :version "1.0.1"
  :description "UML XMI 1.2 repository of the MDA Generator Framework."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "dependencies" "java" "junit" "package" "release"]
- :dependencies [[["junit" "junit" "3.8.1"] :dev]
-                [["org.exolab" "castor" "0.9.5.3" "castor-xml"]]
-                [["xerces" "xercesImpl" "2.9.1"]]]
+ :plugins ["global"
+           ["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/MDDGeneratorPlugin"]
+           ["org.soulspace.baumeister/JavaPlugin"]
+           ["org.soulspace.baumeister/JUnitPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+ :dependencies [["junit/junit, 3.8.1" :dev]
+                ["org.exolab/castor, 0.9.5.3, castor-xml"]
+                ["xerces/xercesImpl, 2.9.1"]]
  ]
