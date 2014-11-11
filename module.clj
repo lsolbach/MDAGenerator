8b2d91b347efdaaaf6678f8154e02a9adbbe3e53
soulman <soulman@pluto>
2014-11-11 10:59:00 +0100

    updated module.clj

--- module.clj
+++ module.clj
@@ -7,7 +7,11 @@
  :version "1.0.1"
  :description "Model repository of the MDA Generator Framework."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "dependencies" "java" "junit" "package" "release"]
- :dependencies [[["org.soulspace.modelling" "MDAXmiRepository" "1.0.1"]]
-                [["junit" "junit" "3.8.1"] :dev]]
+ :plugins ["global"
+           ["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/JavaPlugin"]
+           ["org.soulspace.baumeister/JUnitPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+ :dependencies [["org.soulspace.modelling/MDAXmiRepository, 1.0.1"]
+                ["junit/junit, 3.8.1" :dev]]
  ]
