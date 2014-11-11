a433e68b284f6574ed7adebba08a9d975a2fa0b4
soulman <soulman@pluto>
2014-11-11 10:58:31 +0100

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,14 +4,15 @@
  :type :framework
  :project "org.soulspace.modelling"
  :project-lead "Ludger Solbach"
- :vendor "soulspace.org"
- 
- :description
- "Domain driven design extension for the meta model of the MDA Generator Framework repository.
- This module servers as an example how to extend the meta model."
- 
+ :provider "soulspace.org"
+ :description "Domain driven design extension for the meta model of the MDA Generator Framework repository.
+    This module servers as an example how to extend the meta model."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "dependencies" "java" "package" "release"]
- :dependencies [[["org.soulspace.modelling" "MDARepository" "1.0.1"] :dev]
-                [["org.soulspace.modelling" "MDAXmiRepository" "1.0.1"] :dev]]
+ :plugins ["global"
+           ["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/JavaPlugin"]
+           ["org.soulspace.baumeister/JUnitPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+ :dependencies [["org.soulspace.modelling/MDARepository, 1.0.1" :dev]
+                ["org.soulspace.modelling/MDAXmiRepository, 1.0.1" :dev]]
  ]
